60c36624439d7a6cb302406030acac80ed10e08b
Peter.WM.Wong <Peter.WM.Wong@3439626e-0987-11df-b317-0f3410dc57f6>
2010-01-25 09:21:00 +0000

    * Moved WsRpcService to shared directory * Rebuilt deliverables

--- gwt-websocketrpc/src/org/gwt_websocketrpc/shared/WsRpcService.java
+++ gwt-websocketrpc/src/org/gwt_websocketrpc/shared/WsRpcService.java
@@ -1,8 +1,6 @@
-package org.gwt_websocketrpc.client;
+package org.gwt_websocketrpc.shared;
 
 import com.google.gwt.rpc.client.RpcService;
-
-
 
 /**
  * Marker interface that WebSocket RPC interfaces should extend. 
